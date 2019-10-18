e38c64fd8e359dde0f0f445299d076421b6d3576
magic8baller <lturis@gmail.com>
2019-10-18 14:36:47 -0400

    update image layout - looks betterish

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ export default class App extends Component {
 	state = {
 		image: '',
 		gallery: [],
-		defaultTag: 'kriti'
+		defaultTag: 'kriti',
+
 	}
 
 	componentDidMount () {
@@ -19,6 +20,7 @@ export default class App extends Component {
 				console.log(fetchPhotosResponse.data.resources)
 				this.setState({gallery: fetchPhotosResponse.data.resources})
 			})
+			.catch((error) => console.log(error));
 	}
 
 	onImageDrop (files) {
@@ -27,7 +29,7 @@ export default class App extends Component {
 	}
 
 	handleImageUpload (file) {
-		let upload = request.post("https://api.cloudinary.com/v1_1/ltcloud1/image/c_imagga_crop,w_auto,dpr_auto/q_auto:best, f_auto")
+		let upload = request.post("https://api.cloudinary.com/v1_1/ltcloud1/image/c_imagga_crop")
 			.field('upload_preset', 'ml_default')
 			.field('file', file)
 		upload.end((uploadError, uploadResponse) => {
@@ -44,7 +46,8 @@ export default class App extends Component {
 		window.cloudinary.openUploadWidget(
 			{cloud_name: 'ltcloud1', upload_preset: 'ml_default', tags: [`${this.state.defaultTag}`]},
 			function (err, res) {
-				this.setState({gallery: this.state.gallery.concat(res)});
+				if (res === undefined) return;
+				this.setState({gallery: [...this.state.gallery, res]});
 			}
 		)
 	}
@@ -54,8 +57,10 @@ export default class App extends Component {
 				<div className="FileUpload">
 					<h1>Zine-ish Gallery</h1>
 					<div className="upload">
-						<Button primary floated='right' onClick={this.uploadWidget.bind(this)} className="upload-button">Add New Image!</Button>
+						<Button primary onClick={this.uploadWidget.bind(this)} className="upload-button">Add New Image!</Button>
 					</div>
+					<br />
+					<br />
 					<Home gallery={this.state.gallery} />
 				</div>
 			</div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,41 +2,55 @@ import {CloudinaryContext, Image, Transformation} from 'cloudinary-react';
 import React from 'react';
 import Masonry from 'react-mason';
 import 'semantic-ui-css/semantic.min.css';
-import {Divider, List, Segment} from 'semantic-ui-react';
+import {List} from 'semantic-ui-react';
 import {transformPublicIdToName} from '../utils';
 import './Home.css';
 
 const Home = ({gallery}) => {
 	return (
-		<section className="gallery">
+		<section className="list-container">
 			<CloudinaryContext cloudName="ltcloud1">
 				<Masonry>
 					{gallery.map(data => {
-						let {public_id} = data;
+						let {public_id, created_at} = data;
 						return (
 							<List horizontal>
-								<List.Item>
-									<List.Content>
-										<div className="responsive" key={public_id} >
-											<div className="img">
-												<Segment>
-													<a target="_blank" href={`https://res.cloudinary.com/ltcloud1/image/upload/v1571334056/${public_id}.jpg`}>
-														<Image publicId={public_id} responsive>
-															<Transformation
-																crop="fill"
-																width="820"
-																height="900"
-																gravity="face:center"
-																dpr="auto"
-																placeholder="blank" />
-														</Image>
-													</a>
-													<Divider section />				{transformPublicIdToName(public_id)}
-												</Segment>
+								{/* <List.Item> */}
+								<div className='image-holder'>
+									{/* <List.Content> */}
+									<div className="responsive" key={public_id} >
+										{/* <div className="img"> */}
+										{/* <Segment> */}
+										<a target="_blank" href={`https://res.cloudinary.com/ltcloud1/image/upload/v1571334056/${public_id}.jpg`}>
+											<Image publicId={public_id} responsive>
+												<Transformation
+													crop="fill"
+													width="200"
+													height="200"
+													gravity="face:center"
+													dpr="auto"
+													placeholder="blank" />
+											</Image>
+										</a>
+										<div className="display">
+											<div className="image-name">
+												<div id="name-text">
+													{transformPublicIdToName(public_id)}
+												</div>
+												<div className="image-info">
+													<div className="image-date">
+														{created_at}
+													</div>
+												</div>
 											</div>
 										</div>
-									</List.Content>
-								</List.Item>
+
+									</div>
+
+
+									{/* </List.Content> */}
+								</div>
+								{/* </List.Item> */}
 							</List>
 						)
 					})}
